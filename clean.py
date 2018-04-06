757c616e875c7b198ffda94a96444d46744cb2fb
unnsteinngardars <unnsteinng@gmail.com>
2018-04-06 15:28:18 +0000

    project initialization and starting with creating regex rules

---
+++ clean.py
@@ -0,0 +1,124 @@
+#! /usr/bin/env python
+
+import sys
+import getopt
+import os
+from pathlib import Path
+import re
+import shutil
+import unicodedata
+
+
+def nothing():
+    two = 1 + 1
+    # Function that extracts the name of the TV show from the folder name
+
+
+def getName(directory):
+    name = re.search(
+        r'([\w\d \-\)\(\.\']*)((?= \d\d?. sería))|([\w\d \-\)\(\.\']*)((?= *season))|([\w\d \-\)\(\.\']*)((?= *sería))|([\w\d \-\)\(\.\']*)((?=\.\d\d\d\.))|([\w\d \-\)\(\.\']*)((?= *S\d))', directory, re.IGNORECASE)
+    if name is not None:
+        return name.group()
+    else:
+        return name
+
+
+# Function that extracts the number of the TV show from the folder name
+def getNumber(directory):
+    number = re.search(
+        r'S\d\d?|(?<=season\s)\d\d?|(?<=sería\s)\d\d?', directory, re.IGNORECASE)
+    if number is not None:
+        return number.group()
+    else:
+        return number
+
+
+def main(argv):
+    if len(sys.argv) < 3:
+        raise ValueError(
+            'Invalid argument count\nRun clean.py <source_folder> <destination_folder>')
+    else:
+        source = sys.argv[1]
+        dest = sys.argv[2]
+        if not os.path.isdir(source) or not os.path.isdir(dest):
+            raise ValueError(
+                'Invalid arguments\nRun clean.py <source_folder> <destination_folder>')
+
+    TORRENT_DAY_DOT_COM = 'www.TorrentDay.com'
+    # CWD
+    cwd = os.getcwd()
+    # source path as string and Pathlib object
+    source_path = os.path.join(cwd, source)
+    # destination path as string and Pathlib object
+    dest_path = os.path.join(cwd, dest)
+
+    # Create TEMP Folder for destination content
+    Path(os.path.join(Path(source_path), 'TEMP_FOLDER')).mkdir(exist_ok=True)
+    Path(os.path.join(Path(source_path), 'TEST')).mkdir(exist_ok=True)
+
+    test = os.path.join(source, 'TEST')
+    temp = os.path.join(source, 'TEMP_FOLDER')
+
+    # move test folder to temp folder
+    try:
+        shutil.move(test, temp)
+    except shutil.Error:
+        pass
+        # Search source folder and find tv shows
+
+    for path, dirs, files in os.walk(source):
+        for directory in dirs:
+            # IN FIRST ITERATION:
+            # ONLY CHECK FOR WHOLE SEASONS AND MOVE TO DESIRED LOCATION
+
+            # normalize utf-8 to NFC if needed
+            directory = unicodedata.normalize('NFC', directory)
+            if re.search(r'((sería)|(season)) *\d\d?(?!\-| \-)|(S\d\d?(?![\d\w]))|(?<!\d)(\d\d?[ .]*((season)|(sería)))', directory, re.IGNORECASE):
+                if re.search(r'(?<![ \d\w.])(\d\d?\. *(season|sería))|(?<![ \d\w.])(season|sería) *\d\d?|(?<![ \d\w.])S\d\d?(?![-])', directory, re.IGNORECASE):
+                    # if current directory contains name of show, create folder and move
+                    # if current directory is the source directory, do something later
+
+                    curr_folder_name = str(Path(os.path.join(cwd, path)).name)
+                    curr_path = os.path.join(cwd, path)
+
+                    # print(getNumber(directory))
+                    # print(show)
+                    if curr_path != source_path:
+                        # If the folder name starts with the torrent string then remove it and regex search the rest for the name
+                        if TORRENT_DAY_DOT_COM in directory:
+                            splitted = directory.split('-')
+                            print('SPLITTED')
+                            print(splitted[1])
+
+                        # Create directory for show
+
+                        # Path(os.path.join(Path(curr_path), directory)).mkdir(
+                        #     exist_ok=True)
+
+                    # try:
+                    #     shutil.move(curr_path, source_str + '/TEMP_FOLDER')
+                    # except shutil.Error:
+                    #     print('Directory already exists')
+                    print(directory)
+                else:
+                    print('move\n' + getName(directory) + '\nto new location\n')
+                    nothing()
+                    # if re.search(r'((series)|(sería)|(season))\s*\d\d?\s*\-\s*\d\d?|S\d{2}e\d\d?\s*\-\s*\d\d?', directory, re.IGNORECASE | re.UNICODE):
+                    # print('Folder is a sequence of seasons')
+                    # print(directory)
+                    # print('----------------------------------------')
+
+                    # IN SECOND ITERATION:
+                    # PICK UP ALL REMAINING EPISODE FOLDERS
+                    # If the folder is an episode of a show
+
+                    # if re.search(r'S\d{2}E\d{2}', directory, re.IGNORECASE | re.UNICODE):
+                    # print('Folder is a single episode')
+                    # print(directory)
+                    # print('----------------------------------------')
+                    # TODO// IN SECOND ITERATION PICK UP ALL REMAINING EPISODE FILES
+
+
+
+                    # execute main function
+main(sys.argv)
---
+++ regex-tv-shows.py
@@ -0,0 +1,57 @@
+# Regex match
+import re
+# sample files or folders
+# cd1 cd2
+# subtitle files and folders
+
+# Season folders: season 1, season1-2, season.01, season1, sería, S1, S01
+# Season files: s01e01, ((107, 110) season 1 episode 7 and 10)
+
+
+# Folder that is a single season IGNORES the keyword 'series'
+r'((sería)|(season)) *\d\d?(?!\-| \-)|(S\d\d?(?![\d\w]))|(?<!\d)(\d\d?[ .]*((season)|(sería)))'
+
+# Check if folder only contains season and number
+r'(?<![ \d\w.])(\d\d?\. *(season|sería))|(?<![ \d\w.])(season|sería) *\d\d?|(?<![ \d\w.])S\d\d?(?![-])'
+
+
+# find name of show
+r'([\w\d \-\)\(\.]*)((?= \d\d?. sería))|([\w\d \-\)\(\.]*)((?= *season))|([\w\d \-\)\(\.]*)((?= *sería))|([\w\d \-\)\(\.]*)((?=\.\d\d\d\.))|([\w\d \-\)\(\.]*)((?= *S\d))'
+# Find the number of season
+r'S\d\d?|(?<=season\s)\d\d?|(?<=sería\s)\d\d?'
+
+
+# TEST STRING
+
+Community.S04.
+Magic's Biggest Secrets Finally Revealed season 10
+testseason 7 2313.2
+test season 7 2313.2
+season test season 7 2313.2
+testing sería 10.
+8 Out of 10 Cats - Season 7
+[www.TorrentDay.com] - Spy.2011.S01E02.HDTV.XviD-TLA
+[www.TorrentDay.com] - Spy.2011. season 10
+Spy.2011. season 10
+8.Out.Of.10.Cats.S15E02.HDTV.x264-TLA
+Arrested Development 4. sería - fyrstu 10
+Body.of.Proof.S02E01.HDTV.XviD-LOL.[VTV]
+desperate.housewives.803.hdtv-lol
+Modern Family Season 1-7
+Modern Family Season 1 - 7
+Raising Hope S01
+Dexter Season2-25
+New.Girl.S02.Complete.720p.HDTV.ReEnc-[maximersk]
+Season 12
+Californication S07E1-9
+Californication S07E1 - 9
+Dragons Den S2
+Just a minute series 12-14
+Dragons Den(UK) Season 6
+
+
+# Folder that is a sequence of seasons
+r'((series)|(sería)|(season))\s*\d\d?\s*\-\s*\d\d?|S\d{2}e\d\d?\s*\-\s*\d\d?'
+
+# Folder that is a single episode
+r'S\d{2}E\d{2}'
